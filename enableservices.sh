cb89a2e08ce1ef4be5dfdc665215af024c823b48
amirkh067 <90905503+amirkh067@users.noreply.github.com>
2024-11-22 21:57:37 +0300

    Update enableservices.sh

--- enableservices.sh
+++ enableservices.sh
@@ -2,9 +2,9 @@
 systemctl enable postgresql
 systemctl start postgresql
 systemctl status postgresql
-sytemctl enable snmptrapd
-sytemctl start snmptrapd
-sytemctl status snmptrapd
+systemctl enable snmptrapd
+systemctl start snmptrapd
+systemctl status snmptrapd
 systemctl enable zabbix-proxy
 systemctl start zabbix-proxy
 systemctl status zabbix-proxy
