10cddd1b8ff67be68cfabe7347a736c64acef165
amirkh067 <90905503+amirkh067@users.noreply.github.com>
2024-11-22 11:07:32 +0300

    Add files via upload

---
+++ postgresinstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Step 1: Add PostgreSQL repository to sources list
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Step 2: Import PostgreSQL repository key
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# Step 3: Update apt-get package list
+apt-get update
+
+# Step 4: Install PostgreSQL and PostgreSQL client
+apt install postgresql-15 postgresql-client -y
+
+# Step 5: Switch to the postgres user
+su - postgres -c "psql -c \"ALTER USER postgres PASSWORD 'MonSer09Zbx34Prx';\""
+
+# Step 7: Check PostgreSQL service status
+systemctl status postgresql
