e0c21996637c7fb6424a3601a96589352594f7c2
amirkh067 <90905503+amirkh067@users.noreply.github.com>
2024-12-12 12:07:37 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+/tmp/proxyinstallation/postgresinstall.sh
+sleep 1
+/tmp/proxyinstallation/zabbixproxyinstall.sh
+sleep 1
+/tmp/proxyinstallation/snmptrapsandpsk.sh
+sleep 1
+/tmp/proxyinstallation/odbcdriver.sh
+sleep 1
+/tmp/proxyinstallation/snmptrapdservicefix.sh
+sleep 1
+mv /etc/zabbix/zabbix/zabbix_proxy.conf /etc/zabbix/zabbix/zabbix_proxy.conf_orig
+cp /tmp/proxyinstallation/zabbix_proxy.conf mv /etc/zabbix/zabbix/zabbix_proxy.conf
+/tmp/proxyinstallation/customscriptscommand.sh
+sleep 1
+/tmp/proxyinstallation/enableservices.sh
