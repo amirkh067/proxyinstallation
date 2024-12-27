d191255ec5893a814afc2b77105ab4e1d64c4971
amirkh067 <90905503+amirkh067@users.noreply.github.com>
2024-12-27 15:34:37 +0300

    Update ScriptsPlacingAndEnvChange.sh

--- ScriptsPlacingAndEnvChange.sh
+++ ScriptsPlacingAndEnvChange.sh
@@ -1,2 +1,2 @@
 cp -r /tmp/proxyinstallation/scripts /etc/zabbix
-for file in /usr/lib/zabbix/externalscripts/*.py; do [ -f "$file" ] && sed -i '1s|^#!/usr/bin/python3|#!/opt/zabbix-env/bin/python3|' "$file"; done
+for file in /etc/zabbix/scripts/*.py; do [ -f "$file" ] && sed -i '1s|^#!/usr/bin/python3|#!/opt/zabbix-env/bin/python3|' "$file"; done
